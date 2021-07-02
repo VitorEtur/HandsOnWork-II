38ad587668d78dec75d166a6ebbe78c62a7c1e1d
VitorEtur <61175727+VitorEtur@users.noreply.github.com>
2021-07-02 17:34:07 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+/* Este é o Java Script da pagina. Slide seria o script das imagens
+fazerem o efeito slide no topo do site.
+Já o Subir, seria o scrip para deixar o botão TOPO funcional. */
+
 $('.slide').slick({
     dots: true,
     infinite: true,
